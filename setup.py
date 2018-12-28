3d102d2e77ed18b6c097d0c092009b8cce328f04
landybird <1442172978@qq.com@github.com>
2018-12-28 14:14:41 +0800

    version +

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="landybird",
     author_email="1442172978@qq.com",
     name="hot-magnet",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     url="https://github.com/landybird/hot-magnet",
     py_modules=["hot_magnet"],
