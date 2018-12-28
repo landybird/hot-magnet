b48acec3c424ad218269400f6bf1a8a8305c9ad6
landybird <37992065+landybird@users.noreply.github.com>
2018-12-28 12:18:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
@@ -10,10 +10,12 @@ setup(
     version="0.0.1",
     license="MIT",
     url="https://github.com/landybird/hot-magnet",
-    py_modules=["hot_magnet"],
+    # py_modules=["hot_magnet","handler"],
     install_requires=["setuptools", "requests", "fake_useragent", "requests_html", "PyYAML"],
     description="Get Hot Magnet Top 20",
     long_description = long_description,
+    packages=find_packages(exclude=("tests.*", "tests", "example")),
+    include_package_data=True,
     entry_points={
         "console_scripts": ["hot-magnet=hot_magnet:main"]
     },
